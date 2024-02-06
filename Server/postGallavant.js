18df5cdb495b31e052fd2a82e540ddf50f774626
Max <mas5627@rit.edu>
2024-02-05 20:24:42 -0500

    Added postGallavant.js as our data layer. Added related test file. Created functional DB conne ction and 'pulse check' function. Added dotenv dependency. Added .env file and instructions to set up.

---
+++ Server/postGallavant.js
@@ -0,0 +1,33 @@
+//This file functions as the GeoGallavant Team's custom Data Layer for our PostgreSQL DB
+
+require('dotenv').config()
+const { Pool } = require('pg')
+
+//To set up your PostgreSQL connection:
+// default user: postgres
+// default host: localhost
+// password should have been setup on installation
+// default db: postgres
+// default port: 5432
+
+const PGUSER = process.env.PGUSER
+const PGHOST = process.env.PGHOST
+const PGPASSWORD = process.env.PGPASSWORD
+const PGDATABASE = process.env.PGDATABASE
+const PGPORT = process.env.PGPORT
+
+const pool = new Pool({
+    user: PGUSER,
+    host: PGHOST,
+    database: PGDATABASE,
+    password: PGPASSWORD,
+    port: PGPORT
+  })
+ 
+
+
+async function pulseCheck(){
+    console.log(await pool.query('SELECT NOW()'))
+}
+
+module.exports = {pulseCheck}
---
+++ Server/postGallavant.test.js
@@ -0,0 +1,7 @@
+//This serves as a unit testing file for our Data Layer
+//https://www.w3schools.com/nodejs/ref_assert.asp
+
+const assert = require('assert');
+const pg = require('./postGallavant.js')
+
+pg.test()
