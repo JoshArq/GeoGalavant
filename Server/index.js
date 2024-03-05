7f7876374bd9f43a10c78c696eb58a0260d5a53a
Max Shengelia <mas5627@g.rit.edu>
2024-03-05 16:13:42 -0500

    added token function from prev project, still needs to be assimilated properly.

--- Server/index.js
+++ Server/index.js
@@ -79,3 +79,69 @@ app.listen(PORT, () => {
 function apiLog(msg){
   console.log('\x1b[95m[Server]:\x1b[0m', msg)
 }
+
+
+
+async function generateToken(id, ip = "127.0.0.1"){
+
+  var tokenstr = ""
+
+  //timestamp to base 13
+  var timestamp = Date.now()
+  var cryptTime = timestamp.toString(12)
+
+  //id chunks to base 10
+  var cryptID = id.toString(8)
+
+
+  var cleanIP = ip.replace(/\./g, '')
+  var cleanIP = cleanIP.replace(/\:/g, '')
+  cryptIP = parseInt(cleanIP, 16).toString()
+
+  //get length of longest str
+  var longestStr = cryptTime.length
+  if (cryptID.length>longestStr){longestStr = cryptID.length}
+  if (cryptTime.length>longestStr){longestStr = cryptTime.length}
+
+  //normalize with zeroes
+  cryptID = cryptID.padStart(longestStr, "0")
+  cryptIP = cryptIP.padStart(longestStr, "0")
+  cryptTime = cryptTime.padStart(longestStr, "0")
+
+  var randChars = ""
+  for(let i = 0; i < longestStr; i++){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    randChars += characters.charAt(Math.floor(Math.random() * characters.length));
+     
+  }
+
+  //interleave characters
+  for(let i = 0; i < longestStr; i++){
+    tokenstr += randChars[i];
+    tokenstr += cryptTime[i];
+    //id in reverse order
+    tokenstr += cryptID[longestStr - 1 - i]
+    tokenstr += cryptIP[i]
+  }
+
+
+ //generate 60-char checksum using bcrypt + add to token
+ let myPromise = new Promise((myResolve, myReject) => {
+    bcrypt.genSalt(1,  function(err, salt) {
+      if(err){myReject()}
+      bcrypt.hash(tokenstr, salt, function(err, hash) {
+        if(err){myReject()}
+        var checksumtoken = hash + tokenstr
+        myResolve(checksumtoken)
+      });
+    });
+  });
+
+  await myPromise.then(
+    function(value) {tokenstr = value},
+    function(error) {}
+  );
+  
+  apiLog("Generated token:" + tokenstr)
+  return tokenstr
+}
