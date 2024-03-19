9f44c25e7af0b1a0f03a1408522d5e663eda07db
Max Shengelia <mas5627@g.rit.edu>
2024-03-19 10:12:23 -0400

    tweaked package.json, saved properly this time

--- Server/postGalavant.test.js
+++ Server/postGalavant.test.js
@@ -7,8 +7,34 @@ const pg = require('./postGalavant.js')
 
 async function main(){ 
     // pg.pulseCheck()
-    console.log(await pg.login("bbb", "bbb"))
-    console.log(await pg.login("max123", "password"))
+    
+    //Login tests
+    // console.log(await pg.login("bbb", "bbb"))
+    // console.log(await pg.login("max123", "password"))
+
+    //Customer Creation Tests
+    await pg.createCustomer({
+        "username": "max123",
+        "password": "password",
+        "email": "random@yahoo.com",
+        "appliedbefore": false,
+        "tos": true,
+        "driversLicense": {
+            "firstName": "max",
+            "lastName": "Shengelia",
+            "state": "MO",
+            "ID": "8888888888",
+            "expirationDate": "22/2/27"
+        },
+        "creditCard": {
+            "number": "11111111",
+            "fullName": "Max S",
+            "expirationDate": "11/1/27",
+            "ccv": "666"
+        }
+    });
+
+
 }
 
 main()
