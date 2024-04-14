dea0e09a1b3421fad6de3a8689299a846085e8e1
SJ Miller <sj.miller.tn@gmail.com>
2024-04-14 17:05:11 -0400

    Integrating manager and admin apis (#48)
    * locations add/view on dashboard
    
    * employee handling

--- client/src/components/Pages/Dashboard/Dashboard.js
+++ client/src/components/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,89 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { useState, useEffect } from 'react';
 import Header from '../../GeoHeader/GeoHeader.js';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 
 function AddNewLocation(props) {
+    const [isValid, setIsValid] = useState(true);
+
+    function addLoc() {
+        // Validation
+        let form = document.getElementById("newLoc");
+        let err = document.getElementById("err")
+
+        // Check name
+        if(!form.name.checkValidity()) {
+            err.innerText = "Please add a name"
+            setIsValid(false)
+            return
+        }
+        // Check lat
+        if(!form.lat.checkValidity()) {
+            err.innerText = "Please add a latitude"
+            setIsValid(false)
+            return
+        }
+        // Check lon
+        if(!form.lon.checkValidity()) {
+            err.innerText = "Please add a longitude"
+            setIsValid(false)
+            return
+        }
+        // Check addr
+        if(!form.addr.checkValidity()) {
+            err.innerText = "Please add an address"
+            setIsValid(false)
+            return
+        }
+        // Open
+        if(!form.open.checkValidity() && form.open.value.length == 0) {
+            err.innerText = "Please select an open status"
+            setIsValid(false)
+            return
+        }
+
+        // Create new loc
+        fetch("/api/addLocation", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": props.token
+            },
+            body: JSON.stringify({
+                stationName: form.name.value,
+                address: form.addr.value,
+                isClosed: form.open.value,
+                latitude: form.lat.value,
+                longitude: form.lon.value,
+
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (!data.error) {
+                // Close modal. The page should update to show the new status
+                setIsValid(true)
+                props.onHide()
+            }
+            else {
+                console.log(data.error)
+                err.innerText = "There was an issue submitting your request. Please try again."
+                setIsValid(false)
+            }
+        }).catch(error => {
+            console.log(error)
+            err.innerText = "There was an issue submitting your request. Please try again."
+            setIsValid(false)
+        });
+    }
+
     return (
       <Modal
         {...props}
@@ -22,182 +97,110 @@ function AddNewLocation(props) {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Form>
-                <Form.Group className="mb-3" controlId="latitude">
+            <Alert variant="danger" className={'text-danger bg-danger-subtle' + (isValid ? ' d-none' : '')} id="err">
+                There was an issue loading the data. Please try again.
+            </Alert>
+            <Form id="newLoc">
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Location Name</Form.Label>
+                    <Form.Control type="text" placeholder="Example Location" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="lat">
                     <Form.Label>Latitude</Form.Label>
-                    <Form.Control type="text" placeholder="Enter coordinate" required />
+                    <Form.Control type="number" placeholder="-40.00000" step="0.00001" min="-90.00000" max="90.00000" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="longitude">
+                <Form.Group className="mb-3" controlId="lon">
                     <Form.Label>Longitude</Form.Label>
-                    <Form.Control type="text" placeholder="Enter coordinate" required />
+                    <Form.Control type="number" placeholder="-40.00000" step="0.00001" min="-180.00000" max="180.00000" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="longitude">
-                    <Form.Label>Street Address</Form.Label>
+                <Form.Group className="mb-3" controlId="addr">
+                    <Form.Label>Address</Form.Label>
                     <Form.Control type="text" placeholder="Enter address" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="state">
-                    <Form.Label>State</Form.Label>
-                    <Form.Select name="stateSelect" required>
+                <Form.Group className="mb-3" controlId="open">
+                    <Form.Label>Is the location open?</Form.Label>
+                    <Form.Select name="openSelect" required>
                         <option value="">Select...</option>
-                        <option value="AL">Alabama</option>
-                        <option value="AK">Alaska</option>
-                        <option value="AZ">Arizona</option>
-                        <option value="AR">Arkansas</option>
-                        <option value="CA">California</option>
-                        <option value="CO">Colorado</option>
-                        <option value="CT">Connecticut</option>
-                        <option value="DE">Delaware</option>
-                        <option value="DC">District Of Columbia</option>
-                        <option value="FL">Florida</option>
-                        <option value="GA">Georgia</option>
-                        <option value="HI">Hawaii</option>
-                        <option value="ID">Idaho</option>
-                        <option value="IL">Illinois</option>
-                        <option value="IN">Indiana</option>
-                        <option value="IA">Iowa</option>
-                        <option value="KS">Kansas</option>
-                        <option value="KY">Kentucky</option>
-                        <option value="LA">Louisiana</option>
-                        <option value="ME">Maine</option>
-                        <option value="MD">Maryland</option>
-                        <option value="MA">Massachusetts</option>
-                        <option value="MI">Michigan</option>
-                        <option value="MN">Minnesota</option>
-                        <option value="MS">Mississippi</option>
-                        <option value="MO">Missouri</option>
-                        <option value="MT">Montana</option>
-                        <option value="NE">Nebraska</option>
-                        <option value="NV">Nevada</option>
-                        <option value="NH">New Hampshire</option>
-                        <option value="NJ">New Jersey</option>
-                        <option value="NM">New Mexico</option>
-                        <option value="NY">New York</option>
-                        <option value="NC">North Carolina</option>
-                        <option value="ND">North Dakota</option>
-                        <option value="OH">Ohio</option>
-                        <option value="OK">Oklahoma</option>
-                        <option value="OR">Oregon</option>
-                        <option value="PA">Pennsylvania</option>
-                        <option value="RI">Rhode Island</option>
-                        <option value="SC">South Carolina</option>
-                        <option value="SD">South Dakota</option>
-                        <option value="TN">Tennessee</option>
-                        <option value="TX">Texas</option>
-                        <option value="UT">Utah</option>
-                        <option value="VT">Vermont</option>
-                        <option value="VA">Virginia</option>
-                        <option value="WA">Washington</option>
-                        <option value="WV">West Virginia</option>
-                        <option value="WI">Wisconsin</option>
-                        <option value="WY">Wyoming</option>
+                        {/* The API requires isClosed, so it's reverse of question */}
+                        <option value="false">Yes</option> 
+                        <option value="true">No</option>
                     </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="zipcode">
-                    <Form.Label>Zip Code</Form.Label>
-                    <Form.Control type="text" placeholder="Enter zip" required />
                 </Form.Group>
             </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="" onClick={props.onHide}>Cancel</Button>
-          <Button onClick={props.onHide}>Submit</Button>
+          <Button onClick={() => {addLoc()}}>Submit</Button>
         </Modal.Footer>
       </Modal>
     );
   }
 
-export default function Dashboard() {
-    const [modalShow, setModalShow] = React.useState(false);
+export default function Dashboard({token, isadmin}) {
+    const [modalShow, setModalShow] = useState(false);
+    const [locations, setLocations] = useState([]);
+    const [isValid, setIsValid] = useState(true);
+
+    // Get data
+    useEffect(() => {
+        // Get locations
+        fetch("/api/getLocations", {
+            headers: {
+              "auth-token": token
+            }
+          })
+          .then((res) => res.json())
+          .then((data) => {
+                if(data.error || !data.locations) {
+                    setIsValid(false);
+                    console.log(data.error);
+                }
+                else {
+                    setLocations(data.locations);
+                }
+          }).catch(error => {
+            setIsValid(false);
+            console.log(error);
+          });
+    }, [modalShow])
 
     return (
         <main>
-             <AddNewLocation
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-
+            {isadmin ?
+                <AddNewLocation
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                    token={token}
+                />
+            : '' }
             <Header title="Dashboard" />
             <Container as={'section'}>
-                <Button onClick={() => setModalShow(true)} className="mt-3"><i class="bi bi-plus"></i> Add New Location</Button>
+                <Alert variant="danger" className={'text-danger bg-danger-subtle' + (isValid ? ' d-none' : '')}>
+                    There was an issue loading the data. Please try again.
+                </Alert>
 
-                <Row>
-                    <Col md="4">
-                        <Card className="mt-4 grey-section">
-                            <Card.Body>
-                                <Card.Title as={'h3'} className="fs-4 fw-bold mb-3">
-                                    Gyrogogo Northwest
-                                </Card.Title>
-                                <Card.Text>
-                                    <p>3 Available Cars</p>
-                                    <p>2 Cars Offline</p>
-                                    <p>7 Pick-ups in past month</p>
-                                    <p>6 Drop-offs in past month</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="4">
-                        <Card className="mt-4 grey-section">
-                            <Card.Body>
-                                <Card.Title as={'h3'} className="fs-4 fw-bold mb-3">
-                                    Gyrogogo Northeast
-                                </Card.Title>
-                                <Card.Text>
-                                    <p>3 Available Cars</p>
-                                    <p>2 Cars Offline</p>
-                                    <p>7 Pick-ups in past month</p>
-                                    <p>6 Drop-offs in past month</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="4">
-                        <Card className="mt-4 grey-section">
-                            <Card.Body>
-                                <Card.Title as={'h3'} className="fs-4 fw-bold mb-3">
-                                    Gyrogogo Center City
-                                </Card.Title>
-                                <Card.Text>
-                                    <p>3 Available Cars</p>
-                                    <p>2 Cars Offline</p>
-                                    <p>7 Pick-ups in past month</p>
-                                    <p>6 Drop-offs in past month</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md="4">
-                        <Card className="mt-4 grey-section">
-                            <Card.Body>
-                                <Card.Title as={'h3'} className="fs-4 fw-bold mb-3">
-                                    Gyrogogo Southeast
-                                </Card.Title>
-                                <Card.Text>
-                                    <p>3 Available Cars</p>
-                                    <p>2 Cars Offline</p>
-                                    <p>7 Pick-ups in past month</p>
-                                    <p>6 Drop-offs in past month</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="4">
-                        <Card className="mt-4 grey-section">
-                            <Card.Body>
-                                <Card.Title as={'h3'} className="fs-4 fw-bold mb-3">
-                                    Gyrogogo Airport
-                                </Card.Title>
-                                <Card.Text>
-                                    <p>3 Available Cars</p>
-                                    <p>2 Cars Offline</p>
-                                    <p>7 Pick-ups in past month</p>
-                                    <p>6 Drop-offs in past month</p>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                <Button onClick={() => setModalShow(true)} className="mt-3" disabled={!isadmin}><i class="bi bi-plus"></i> Add New Location</Button>
+
+                <Row className="p-4">
+                    {locations.map((loc) => {return(
+                        <Col sm="6" lg="4">
+                            <Card className="mt-4 grey-section">
+                                <Card.Body>
+                                    <Card.Title as={'h3'} className="fs-4 fw-bold mb-3">
+                                        {loc.name}
+                                    </Card.Title>
+                                    <Card.Text>
+                                        <p>{loc.address}</p>
+                                        <hr className="border-2 border-dark" />
+                                        <p>? Available Cars</p>
+                                        <p>? Cars Offline</p>
+                                        <p>? Pick-ups in past month</p>
+                                        <p>? Drop-offs in past month</p>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )})}
                 </Row>
             </Container> 
         </main>
